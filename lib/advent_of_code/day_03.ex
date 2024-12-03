6153de55a156bc2dbc9469d2d642a3e5d61211da
Erik Pihl <pihl@primea.se>
2024-12-03 21:04:12 +0100

    Day 3 part 2

--- lib/advent_of_code/day_03.ex
+++ lib/advent_of_code/day_03.ex
@@ -1,48 +1,60 @@
 defmodule AdventOfCode.Day03 do
   def part1(args) do
-    parser1(args, {:nop}, 0)
+    parser1(args, {:nop}, 0, :enabled)
   end
 
-  def parser1(<<>>, _, result) do
+  def part2(args) do
+    parser1(args, {:nop}, 0, :enabled)
+  end
+
+  def parser1(<<>>, _, result, _) do
     result
   end
 
-  def parser1(<<"mul(", rest::binary>>, _, result) do
-    parser1(rest, {:mul, []}, result)
+  def parser1(<<"do()", rest::binary>>, _, result, _) do
+    parser1(rest, {:nop}, result, :enabled)
   end
 
-  def parser1(<<")", rest::binary>>, {:mul, x, y}, result) when x != [] and y != [] do
+
+  def parser1(<<"don't()", rest::binary>>, _, result, _) do
+    parser1(rest, {:nop}, result, :disabled)
+  end
+
+  def parser1(<<"mul(", rest::binary>>, _, result, state) do
+    parser1(rest, {:mul, []}, result, state)
+  end
+
+  def parser1(<<")", rest::binary>>, {:mul, x, y}, result, :enabled) when x != [] and y != [] do
     parser1(
       rest,
       {:nop},
-      List.to_integer(Enum.reverse(x)) * List.to_integer(Enum.reverse(y)) + result
+      List.to_integer(Enum.reverse(x)) * List.to_integer(Enum.reverse(y)) + result,
+      :enabled
     )
   end
 
-  def parser1(<<",", rest::binary>>, {:mul, b1}, result) when b1 != [] do
-    parser1(rest, {:mul, b1, []}, result)
+  def parser1(<<",", rest::binary>>, {:mul, b1}, result, state) when b1 != [] do
+    parser1(rest, {:mul, b1, []}, result, state)
   end
 
-  def parser1(<<char, rest::binary>>, {:mul, b1}, result) do
+  def parser1(<<char, rest::binary>>, {:mul, b1}, result, state) do
     if char >= 48 and char <= 57 do
-      parser1(rest, {:mul, [char | b1]}, result)
+      parser1(rest, {:mul, [char | b1]}, result, state)
     else
-      parser1(rest, {:nop}, result)
+      parser1(rest, {:nop}, result, state)
     end
   end
 
-  def parser1(<<char, rest::binary>>, {:mul, b1, b2}, result) do
+  def parser1(<<char, rest::binary>>, {:mul, b1, b2}, result, state) do
     if char >= 48 and char <= 57 do
-      parser1(rest, {:mul, b1, [char | b2]}, result)
+      parser1(rest, {:mul, b1, [char | b2]}, result, state)
     else
-      parser1(rest, {:nop}, result)
+      parser1(rest, {:nop}, result,state)
     end
   end
 
-  def parser1(<<_, rest::binary>>, _, result) do
-    parser1(rest, {:nop}, result)
+  def parser1(<<_, rest::binary>>, _, result, state) do
+    parser1(rest, {:nop}, result, state)
   end
 
-  def part2(_args) do
-  end
 end
--- test/advent_of_code/day_03_test.exs
+++ test/advent_of_code/day_03_test.exs
@@ -11,11 +11,9 @@ defmodule AdventOfCode.Day03Test do
     assert result == 161
   end
 
-  @tag :skip
   test "part2" do
-    input = nil
-    result = part2(input)
+    result = part2("xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))")
 
-    assert result
+    assert result == 48
   end
 end
