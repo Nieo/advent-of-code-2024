0bcfeb50805ea35565c4a2a93178209308c1c4b0
Erik Pihl <pihl@primea.se>
2024-12-03 20:49:47 +0100

    Day 3 part 1

--- lib/advent_of_code/day_03.ex
+++ lib/advent_of_code/day_03.ex
@@ -1,5 +1,46 @@
 defmodule AdventOfCode.Day03 do
-  def part1(_args) do
+  def part1(args) do
+    parser1(args, {:nop}, 0)
+  end
+
+  def parser1(<<>>, _, result) do
+    result
+  end
+
+  def parser1(<<"mul(", rest::binary>>, _, result) do
+    parser1(rest, {:mul, []}, result)
+  end
+
+  def parser1(<<")", rest::binary>>, {:mul, x, y}, result) when x != [] and y != [] do
+    parser1(
+      rest,
+      {:nop},
+      List.to_integer(Enum.reverse(x)) * List.to_integer(Enum.reverse(y)) + result
+    )
+  end
+
+  def parser1(<<",", rest::binary>>, {:mul, b1}, result) when b1 != [] do
+    parser1(rest, {:mul, b1, []}, result)
+  end
+
+  def parser1(<<char, rest::binary>>, {:mul, b1}, result) do
+    if char >= 48 and char <= 57 do
+      parser1(rest, {:mul, [char | b1]}, result)
+    else
+      parser1(rest, {:nop}, result)
+    end
+  end
+
+  def parser1(<<char, rest::binary>>, {:mul, b1, b2}, result) do
+    if char >= 48 and char <= 57 do
+      parser1(rest, {:mul, b1, [char | b2]}, result)
+    else
+      parser1(rest, {:nop}, result)
+    end
+  end
+
+  def parser1(<<_, rest::binary>>, _, result) do
+    parser1(rest, {:nop}, result)
   end
 
   def part2(_args) do
--- test/advent_of_code/day_03_test.exs
+++ test/advent_of_code/day_03_test.exs
@@ -1,14 +1,14 @@
 defmodule AdventOfCode.Day03Test do
   use ExUnit.Case
 
+  @input "xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))"
+
   import AdventOfCode.Day03
 
-  @tag :skip
   test "part1" do
-    input = nil
-    result = part1(input)
+    result = part1(@input)
 
-    assert result
+    assert result == 161
   end
 
   @tag :skip
