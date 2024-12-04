f52eb6d5ca45ed3f362f04c6b16ee315c21fe12c
Erik Pihl <pihl@primea.se>
2024-12-04 13:52:47 +0100

    Day 4 part 2

--- lib/advent_of_code/day_03.ex
+++ lib/advent_of_code/day_03.ex
@@ -1,31 +1,30 @@
 defmodule AdventOfCode.Day03 do
   def part1(args) do
-    parser1(args, {:nop}, 0, :enabled)
+    parser(args, {:nop}, 0, :enabled)
   end
 
   def part2(args) do
-    parser1(args, {:nop}, 0, :enabled)
+    parser(args, {:nop}, 0, :enabled)
   end
 
-  def parser1(<<>>, _, result, _) do
+  def parser(<<>>, _, result, _) do
     result
   end
 
-  def parser1(<<"do()", rest::binary>>, _, result, _) do
-    parser1(rest, {:nop}, result, :enabled)
+  def parser(<<"do()", rest::binary>>, _, result, _) do
+    parser(rest, {:nop}, result, :enabled)
   end
 
-
-  def parser1(<<"don't()", rest::binary>>, _, result, _) do
-    parser1(rest, {:nop}, result, :disabled)
+  def parser(<<"don't()", rest::binary>>, _, result, _) do
+    parser(rest, {:nop}, result, :disabled)
   end
 
-  def parser1(<<"mul(", rest::binary>>, _, result, state) do
-    parser1(rest, {:mul, []}, result, state)
+  def parser(<<"mul(", rest::binary>>, _, result, state) do
+    parser(rest, {:mul, []}, result, state)
   end
 
-  def parser1(<<")", rest::binary>>, {:mul, x, y}, result, :enabled) when x != [] and y != [] do
-    parser1(
+  def parser(<<")", rest::binary>>, {:mul, x, y}, result, :enabled) when x != [] and y != [] do
+    parser(
       rest,
       {:nop},
       List.to_integer(Enum.reverse(x)) * List.to_integer(Enum.reverse(y)) + result,
@@ -33,28 +32,27 @@ defmodule AdventOfCode.Day03 do
     )
   end
 
-  def parser1(<<",", rest::binary>>, {:mul, b1}, result, state) when b1 != [] do
-    parser1(rest, {:mul, b1, []}, result, state)
+  def parser(<<",", rest::binary>>, {:mul, b1}, result, state) when b1 != [] do
+    parser(rest, {:mul, b1, []}, result, state)
   end
 
-  def parser1(<<char, rest::binary>>, {:mul, b1}, result, state) do
+  def parser(<<char, rest::binary>>, {:mul, b1}, result, state) do
     if char >= 48 and char <= 57 do
-      parser1(rest, {:mul, [char | b1]}, result, state)
+      parser(rest, {:mul, [char | b1]}, result, state)
     else
-      parser1(rest, {:nop}, result, state)
+      parser(rest, {:nop}, result, state)
     end
   end
 
-  def parser1(<<char, rest::binary>>, {:mul, b1, b2}, result, state) do
+  def parser(<<char, rest::binary>>, {:mul, b1, b2}, result, state) do
     if char >= 48 and char <= 57 do
-      parser1(rest, {:mul, b1, [char | b2]}, result, state)
+      parser(rest, {:mul, b1, [char | b2]}, result, state)
     else
-      parser1(rest, {:nop}, result,state)
+      parser(rest, {:nop}, result, state)
     end
   end
 
-  def parser1(<<_, rest::binary>>, _, result, state) do
-    parser1(rest, {:nop}, result, state)
+  def parser(<<_, rest::binary>>, _, result, state) do
+    parser(rest, {:nop}, result, state)
   end
-
 end
--- lib/advent_of_code/day_04.ex
+++ lib/advent_of_code/day_04.ex
@@ -1,5 +1,47 @@
 defmodule AdventOfCode.Day04 do
-  def part1(_args) do
+  def part1(args) do
+    args
+    |> parse(0, 0, %{})
+    |> search
+  end
+
+  def parse(<<>>, _, _, result) do
+    result
+  end
+
+  def parse(<<"\n", rest::binary>>, row, _, result) do
+    parse(rest, row + 1, 0, result)
+  end
+
+  def parse(<<c, rest::binary>>, row, col, result) do
+    parse(rest, row, col + 1, Map.put(result, %{r: row, c: col}, List.to_string([c])))
+  end
+
+  def search(data) do
+    data
+    |> Enum.filter(fn {_, v} -> v == "X" end)
+    |> Enum.map(fn {k, _} -> search_x(k, data) end)
+    |> Enum.sum
+  end
+
+  def search_x(%{r: r, c: c}, grid) do
+    for x <- -1..1, y <- -1..1 do
+      search_next(["M", "A", "S"], %{r: r + x, c: c + y}, {x, y}, grid)
+    end
+    |> Enum.sum()
+  end
+
+  def search_next([l | rest], %{r: r, c: c} = k, {x, y}, grid) do
+    case Map.get(grid, k) == l do
+      true ->
+        case rest == [] do
+          true -> 1
+          false -> search_next(rest, %{r: r + x, c: c + y}, {x, y}, grid)
+        end
+
+      false ->
+        0
+    end
   end
 
   def part2(_args) do
