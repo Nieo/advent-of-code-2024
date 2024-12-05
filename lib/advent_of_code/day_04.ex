118a91a80e8089df6815ae5711fbb2265208fc03
Erik Pihl <pihl@primea.se>
2024-12-05 08:33:25 +0100

    Day 4 part 2

--- lib/advent_of_code/day_04.ex
+++ lib/advent_of_code/day_04.ex
@@ -21,7 +21,7 @@ defmodule AdventOfCode.Day04 do
     data
     |> Enum.filter(fn {_, v} -> v == "X" end)
     |> Enum.map(fn {k, _} -> search_x(k, data) end)
-    |> Enum.sum
+    |> Enum.sum()
   end
 
   def search_x(%{r: r, c: c}, grid) do
@@ -44,6 +44,29 @@ defmodule AdventOfCode.Day04 do
     end
   end
 
-  def part2(_args) do
+  def part2(args) do
+    args
+    |> parse(0, 0, %{})
+    |> search2
   end
+
+  def search2(data) do
+    data
+    |> Enum.filter(fn {_, v} -> v == "A" end)
+    |> Enum.count(fn {k, _} -> search_a(k, data) end)
+    # |> dbg
+  end
+
+  def search_a(%{r: r, c: c}, data) do
+    [{1, 1}, {1, -1}, {-1, -1}, {-1, 1}]
+    |> Enum.map(fn {x, y} -> Map.get(data, %{r: r + x, c: c + y}) end)
+    |> is_x()
+    # |> dbg
+  end
+
+  def is_x(["M", "M", "S", "S"]), do: true
+  def is_x(["S", "M", "M", "S"]), do: true
+  def is_x(["S", "S", "M", "M"]), do: true
+  def is_x(["M", "S", "S", "M"]), do: true
+  def is_x(_), do: false
 end
--- test/advent_of_code/day_04_test.exs
+++ test/advent_of_code/day_04_test.exs
@@ -2,31 +2,27 @@ defmodule AdventOfCode.Day04Test do
   use ExUnit.Case
 
   import AdventOfCode.Day04
-
+  @input  """
+  MMMSXXMASM
+  MSAMXMSMSA
+  AMXSXMAAMM
+  MSAMASMSMX
+  XMASAMXAMM
+  XXAMMXXAMA
+  SMSMSASXSS
+  SAXAMASAAA
+  MAMMMXMMMM
+  MXMXAXMASX
+  """
   test "part1" do
-    input = """
-    MMMSXXMASM
-    MSAMXMSMSA
-    AMXSXMAAMM
-    MSAMASMSMX
-    XMASAMXAMM
-    XXAMMXXAMA
-    SMSMSASXSS
-    SAXAMASAAA
-    MAMMMXMMMM
-    MXMXAXMASX
-    """
-
-    result = part1(input)
+    result = part1(@input)
 
     assert result == 18
   end
 
-  @tag :skip
   test "part2" do
-    input = nil
-    result = part2(input)
+    result = part2(@input)
 
-    assert result
+    assert result == 9
   end
 end
